29fadba197bfca1b0675a163faf693f8ebcb621e
Mike Sausa <mikejsausa@gmail.com>
2016-12-14 17:36:48 -0700

    added credentials file

---
+++ auth0-credentials.js
@@ -0,0 +1,4 @@
+module.exports = {
+  clientId: "WFJMNOHWY0fSVEJ1ODojgVaOothC1V7C",
+  domain: "song-analyzer.auth0.com"
+};
---
+++ ios/Pods/Target Support Files/Pods-Song_Analyzer/Pods-Song_Analyzer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Song_Analyzer : NSObject
+@end
+@implementation PodsDummy_Pods_Song_Analyzer
+@end
