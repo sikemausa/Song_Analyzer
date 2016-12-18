f6523685b2098a9507d7399ca93ed8aebc473f7a
Mike Sausa <mikejsausa@gmail.com>
2016-12-18 13:54:56 -0700

    added ability to edit user info

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,6 +3,8 @@ import { StyleSheet, Image, Dimensions, Platform, Text, View, Switch, Navigator,
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import userContainer from '../containers/userContainer';
 import tokenContainer from '../containers/tokenContainer';
+import Auth0 from 'react-native-auth0';
+const auth0 = new Auth0('https://song-analyzer.auth0.com');
 
 class Profile extends Component {
   constructor (props) {
@@ -11,28 +13,28 @@ class Profile extends Component {
 
   render() {
     const { user, token } = this.props;
-    let firstName = user.extraInfo.given_name;
     console.log(this.props);
+    let firstName = user.extraInfo.given_name;
       return (
         <View>
-            <Text>Thanks for dropping in {firstName}!</Text>
-          {/* <Image
+          <TouchableHighlight onPress={() => this.updateUserInfo()}>
+          <Text>Thanks for dropping in {firstName}!</Text>
+          </TouchableHighlight>
+          <Image
             style={{width: 50, height: 50}}
             source={{uri: user.picture}}
-          /> */}
+          />
         </View>
       );
   }
 
-  // updateUserInfo() {
-  //   const { user } = this.props;
-  //   let auth0Endpoint = `song-analyzer.auth0.com/api/users/${user.userId}`;
-  //   fetch(auth0Endpoint, {
-  //     method: "PATCH",
-  //     body: `{ user_metadata: { addresses: { home: '123 Main Street, Anytown, ST 12345' } } }`,
-  //     Content-Type
-  //   })
-  // }
+  updateUserInfo() {
+    const { user, token } = this.props;
+    auth0.users(`${token.idToken}`)
+    .patch(`${user.userId}`, {'first_name': 'John', 'last_name': 'Doe'})
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+  }
 }
 
 
